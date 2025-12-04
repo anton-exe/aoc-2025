834aa74c335cfe33e15f4b4ed981d268c72de3f4
Anton <anton@anton-exe.eu>
2025-12-04 08:27:13 +0000

    day 4: i don't have a joke this just sucks

---
+++ src/day04.rs
@@ -0,0 +1,132 @@
+pub fn generator(input: &str) -> Vec<Vec<bool>> {
+    input
+        .lines()
+        .map(|line| {
+            line.as_bytes()
+                .iter()
+                .map(|byte| {
+                    if byte == &b'@' {
+                        return true;
+                    }
+                    false
+                })
+                .collect()
+        })
+        .collect()
+}
+
+const NEIGHBOURS: [(i32, i32); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
+pub fn part_1(input: &[Vec<bool>]) -> u64 {
+    input
+        .iter()
+        .enumerate()
+        .map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .map(|(x, cell)| {
+                    if *cell
+                        && NEIGHBOURS
+                            .iter()
+                            .map(|(dy, dx)| {
+                                if ((y == 0) && (*dy < 0))
+                                    || ((x == 0) && (*dx < 0))
+                                    || (!input
+                                        .get(
+                                            usize::try_from(
+                                                i32::try_from(y).unwrap_or_default() + dy,
+                                            )
+                                            .unwrap_or_default(),
+                                        )
+                                        .unwrap_or(&Vec::<bool>::new())
+                                        .get(
+                                            usize::try_from(
+                                                i32::try_from(x).unwrap_or_default() + dx,
+                                            )
+                                            .unwrap_or_default(),
+                                        )
+                                        .unwrap_or(&false))
+                                {
+                                    return 0;
+                                }
+                                1
+                            })
+                            .sum::<i64>()
+                            < 4
+                    {
+                        return 1;
+                    }
+                    0
+                })
+                .sum::<u64>()
+        })
+        .sum()
+}
+
+pub fn part_2(input: &[Vec<bool>]) -> u64 {
+    let mut total = 0;
+
+    let mut grid = input.to_vec();
+
+    loop {
+        let og_grid = grid.clone();
+        let sum = og_grid
+            .iter()
+            .enumerate()
+            .map(|(y, row)| {
+                row.iter()
+                    .enumerate()
+                    .map(|(x, cell)| {
+                        if *cell
+                            && NEIGHBOURS
+                                .iter()
+                                .map(|(dy, dx)| {
+                                    if ((y == 0) && (*dy < 0))
+                                        || ((x == 0) && (*dx < 0))
+                                        || (!og_grid
+                                            .get(
+                                                usize::try_from(
+                                                    i32::try_from(y).unwrap_or_default() + dy,
+                                                )
+                                                .unwrap_or_default(),
+                                            )
+                                            .unwrap_or(&Vec::<bool>::new())
+                                            .get(
+                                                usize::try_from(
+                                                    i32::try_from(x).unwrap_or_default() + dx,
+                                                )
+                                                .unwrap_or_default(),
+                                            )
+                                            .unwrap_or(&false))
+                                    {
+                                        return 0;
+                                    }
+                                    1
+                                })
+                                .sum::<i64>()
+                                < 4
+                        {
+                            grid[y][x] = false;
+                            return 1;
+                        }
+                        0
+                    })
+                    .sum::<u64>()
+            })
+            .sum::<u64>();
+        if sum == 0 {
+            break;
+        }
+        total += sum;
+    }
+    total
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 aoc_main::main! {
     year 2025;
     day01 : generator => part_1, part_2;
     day02 : generator => part_1, part_2;
     day03 : generator => part_1, part_1_but_like_part_2, part_2;
+    day04 : generator => part_1, part_2;
 }
